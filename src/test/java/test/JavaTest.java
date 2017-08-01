1e3ea9ace1a3f502bb8f9b4bd2f515b1aeb4be88
guoke <1127090290@qq.com>
2017-08-01 17:00:45 +0800

    添加redis 配置，并配置配置文件的测试环境。

--- src/test/java/test/JavaTest.java
+++ src/test/java/test/JavaTest.java
@@ -5,44 +5,26 @@ import com.qibao.model.StudentMapper;
 import com.qibao.model.dao.Student;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import redis.clients.jedis.Jedis;
+import test.common.BaseContextCase;
 
 import java.math.BigDecimal;
 
 /**
  * Created by giozola on 2017/6/26.
  */
-public class JavaTest {
+public class JavaTest extends BaseContextCase{
     @Autowired
     StudentMapper studentMapper;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @Test
-    public void testOut(){
-        BigDecimal shouru = new BigDecimal(1000);
-        BigDecimal lilv = new BigDecimal(0.05);
-        BigDecimal touzi = shouru;
-        for (int i= 0;i<120 ;i++){
-            BigDecimal benxi = benxi(touzi,lilv);
-            if ((i+1)%12 == 0){
-                System.out.println("第"+(i+1)/12+"年底的本溪为："+benxi);
-                System.out.println("        利息为："+lixi(touzi,lilv));
-            }
-            touzi = benxi.add(shouru);
-        }
-    }
-
-    /**
-     * 月底的利息收入
-     * @param touzi
-     * @param lilv
-     * @return
-     */
-    public BigDecimal lixi(BigDecimal touzi,BigDecimal lilv){
-        return touzi.multiply(lilv).setScale(2,BigDecimal.ROUND_HALF_UP);
-    }
-    /**
-     * 每月月底的利息+本金
-     */
-    public BigDecimal benxi(BigDecimal touzi,BigDecimal lilv){
-         return  touzi.add(lixi(touzi,lilv));
+    public void testRedis(){
+        ValueOperations<String,String> valueOperations= redisTemplate.opsForValue();
+        valueOperations.set("valueOperaations","this is value");
+        System.out.println(valueOperations.get("valueOperaations"));
     }
 }
