11f7d31f2b9d3adcf1a116b060d9f65a7a7bd47a
guoke <1127090290@qq.com>
2017-08-02 18:47:19 +0800

    房贷计算器，新建类

---
+++ src/main/java/com/core/util/HouseMortgageCalculate.java
@@ -0,0 +1,28 @@
+package com.core.util;
+
+/**
+ * Created by giozola on 2017/8/2.
+ */
+//房贷计算器
+
+import java.math.BigDecimal;
+
+/**
+ * 等额本息法
+ */
+public class HouseMortgageCalculate {
+    /**
+     * 本金
+     */
+    private BigDecimal principal;
+    /**
+     * 年利率
+     */
+    private BigDecimal InterestRateYear;
+    /**
+     * 还款月数
+     */
+    private int repaymentTimeMonth;
+
+
+}
