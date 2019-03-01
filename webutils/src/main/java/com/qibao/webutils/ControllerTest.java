e8160ac58b02aea8d766db0e22c728816dc1f456
guoke <1127090290@qq.com>
2019-03-01 17:04:10 +0800

    freemarker 模板引擎添加

--- webutils/src/main/java/com/qibao/webutils/ControllerTest.java
+++ webutils/src/main/java/com/qibao/webutils/ControllerTest.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 启动测试类
@@ -18,13 +21,13 @@ import java.util.Date;
  * @Description:
  * @author: weichao 2019/2/21 17 25
  */
-@RestController
+@Controller
 public class ControllerTest {
     Logger logger = LoggerFactory.getLogger(ControllerTest.class);
 
     @RequestMapping("/test")
-    public String test(){
-        logger.info("test Controller");
-        return  "index12";
+    public String  test(){
+        logger.info("test");
+        return "index";
     }
 }
